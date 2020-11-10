fe77e180c5aeb68f110d539ec31b469bd970ff44
Yargo Valério dos Santos Abou Ali <f202367@intrajus.tjrn>
2020-11-10 12:55:06 -0300

    estilizando a dashboard e chamando a api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import api from '../../services/api';
+import logoImg from '../../assets/tjrn.png';
 
-import { Title, Header } from './styles';
+import { Title, Header, Body, Form, Books, Copyright } from './styles';
 
-interface Book {
+interface Books {
   results: {
     list_name: string;
+    display_name: string;
+    list_name_encoded: string;
     oldest_published_date: string;
+    newest_published_date: string;
+    updated: string;
   };
 }
-
 const Dashboard: React.FC = () => {
-  const [books, setBooks] = useState<Book[]>([]);
+  const [searchBook, setSearchBook] = useState('');
+  const [books, setBooks] = useState<Books[]>([]);
 
-  const response = api.get(
-    `/lists/names.json?api-key=rDoarHAbEEVJo0xbnk97zSVuSYt6zSf3`,
-  );
+  async function handleSearchBook(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
 
-  const book = response;
+    const response = await api.get<Books>(
+      `lists/names.json?api-key=rDoarHAbEEVJo0xbnk97zSVuSYt6zSf3`,
+    );
 
-  setBooks([...books, book]);
-
+    setBooks([...books, response.data]);
+  }
   return (
     <>
       <Header>
         <div>
+          <img src={logoImg} alt="TJRN" />
           <Title>Dashboard - SETIC</Title>
         </div>
       </Header>
-      {/* <p>{response}</p> */}
+      <Body>
+        <Form onSubmit={handleSearchBook}>
+          <input
+            value={searchBook}
+            onChange={e => setSearchBook(e.target.value)}
+            placeholder="Pesquise aqui pelo livro"
+          />
+          <button type="submit">Pesquisar</button>
+        </Form>
+        <Copyright>
+          <strong>
+            Copyright (c) 2020 The New York Times Company. All Rights Reserved.
+          </strong>
+          <strong>Results Found: 59</strong>
+        </Copyright>
+
+        <hr />
+        <Books>
+          {books.map(book => (
+            <a key={book.results.list_name} href="teste">
+              <div>
+                <strong>{book.results.display_name}</strong>
+                <p>
+                  <small>{book.results.list_name_encoded}</small>
+                </p>
+              </div>
+              <div>
+                <p>
+                  Newest publish Date:
+                  {book.results.newest_published_date}
+                </p>
+                <p>
+                  Oldest publish Date:
+                  {book.results.oldest_published_date}
+                </p>
+              </div>
+              <p>
+                Updated:
+                {book.results.updated}
+              </p>
+            </a>
+          ))}
+        </Books>
+      </Body>
     </>
   );
 };
