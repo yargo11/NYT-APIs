dccd32acddca23adf5cfbb08d08c27237c22aff0
Yargo Valério dos Santos Abou Ali <f202367@intrajus.tjrn>
2020-11-12 12:55:05 -0300

    tentando usar useEffect

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,48 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import api from '../../services/api';
 import logoImg from '../../assets/tjrn.png';
 
 import { Title, Header, Body, Form, Books, Copyright } from './styles';
 
+interface Livro {
+  display_name: string;
+  oldest_published_date: string;
+  newest_published_date: string;
+  updated: string;
+}
 interface Books {
   results: {
-    list_name: string;
-    display_name: string;
     list_name_encoded: string;
-    oldest_published_date: string;
-    newest_published_date: string;
-    updated: string;
   };
+  livro: Livro;
 }
 const Dashboard: React.FC = () => {
-  const [searchBook, setSearchBook] = useState('');
+  // const [searchBook, setSearchBook] = useState('');
   const [books, setBooks] = useState<Books[]>([]);
+  const apikey = '?api-key=rDoarHAbEEVJo0xbnk97zSVuSYt6zSf3';
 
-  async function handleSearchBook(
-    event: FormEvent<HTMLFormElement>,
-  ): Promise<void> {
-    event.preventDefault();
+  // async function handleSearchBook(
+  //   event: FormEvent<HTMLFormElement>,
+  // ): Promise<void> {
+  //   event.preventDefault();
 
-    const response = await api.get<Books>(
-      `lists/names.json?api-key=rDoarHAbEEVJo0xbnk97zSVuSYt6zSf3`,
-    );
+  //   const response = await api.get<Books>(`lists/names.json${apikey}`);
 
-    setBooks([...books, response.data]);
-  }
+  //   // setBooks([response.data]);
+  // }
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  const loadBooks = async () => {
+    const response = await api.get<Books>(`lists/names.json${apikey}`);
+
+    setBooks([response.data]);
+  };
+
+  console.log(books);
+
   return (
     <>
       <Header>
@@ -38,10 +52,11 @@ const Dashboard: React.FC = () => {
         </div>
       </Header>
       <Body>
-        <Form onSubmit={handleSearchBook}>
+        {/* <Form onSubmit={handleSearchBook}> */}
+        <Form>
           <input
-            value={searchBook}
-            onChange={e => setSearchBook(e.target.value)}
+            // value={searchBook}
+            // onChange={e => setSearchBook(e.target.value)}
             placeholder="Pesquise aqui pelo livro"
           />
           <button type="submit">Pesquisar</button>
@@ -56,9 +71,9 @@ const Dashboard: React.FC = () => {
         <hr />
         <Books>
           {books.map(book => (
-            <a key={book.results.list_name} href="teste">
+            <a key={book.results.list_name_encoded} href="teste">
               <div>
-                <strong>{book.results.display_name}</strong>
+                <strong>{book.livro.display_name}</strong>
                 <p>
                   <small>{book.results.list_name_encoded}</small>
                 </p>
@@ -66,16 +81,16 @@ const Dashboard: React.FC = () => {
               <div>
                 <p>
                   Newest publish Date:
-                  {book.results.newest_published_date}
+                  {book.livro.newest_published_date}
                 </p>
                 <p>
                   Oldest publish Date:
-                  {book.results.oldest_published_date}
+                  {book.livro.oldest_published_date}
                 </p>
               </div>
               <p>
                 Updated:
-                {book.results.updated}
+                {book.livro.updated}
               </p>
             </a>
           ))}
